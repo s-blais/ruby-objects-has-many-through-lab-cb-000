5e21355af4b4b10e92eccc12bf37492b463ee0fb
Stephen Blais <stephen@s-blais.com>
2019-09-04 16:01:49 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,20 @@
+class Appointment
+
+  attr_accessor :doctor, :patient, :date
+
+  @@all = []
+
+def intialize(patient, doctor, date)
+  @patient = patient
+  @doctor = doctor
+  @date = date
+  @@all << self
+end
+  
+  def self.all
+    @@all
+  end
+
+
+
+end
