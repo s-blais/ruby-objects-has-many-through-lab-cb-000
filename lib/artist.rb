d7ff737e94ce4aa5df022cf9aa6c91178b42659a
Stephen Blais <stephen@s-blais.com>
2019-09-04 15:37:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,10 +13,8 @@ class Artist
     @@all
   end
 
-  def new_song(name)
-    song = Song.new
-    #song.name = name
-    song.artist = self
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
   end
 
   def songs
